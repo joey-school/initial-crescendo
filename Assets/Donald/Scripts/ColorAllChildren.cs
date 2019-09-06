885296c40444b67ed0a7ec9319c54c719672a5dc
DOverbeeke <donaldoverbeeke@hotmail.com>
2019-09-06 16:02:06 +0200

    editing without running game options menu navigation

--- Assets/Donald/Scripts/ColorAllChildren.cs
+++ Assets/Donald/Scripts/ColorAllChildren.cs
@@ -2,23 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class ColorAllChildren : MonoBehaviour
 {
     public Color Color;
 
+    private Color prevColor;
+
     // Start is called before the first frame update
     void Start()
     {
-        Transform[] childrenTransforms = GetComponentsInChildren<Transform>();
-        foreach(Transform t in childrenTransforms)
-        {
-            t.GetComponentInChildren<SpriteRenderer>().color = Color;
-        }
+        prevColor = Color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(prevColor != Color)
+        {
+            Transform[] childrenTransforms = GetComponentsInChildren<Transform>();
+            foreach (Transform t in childrenTransforms)
+            {
+                t.GetComponentInChildren<SpriteRenderer>().color = Color;
+            }
+            prevColor = Color;
+        }
     }
 }
