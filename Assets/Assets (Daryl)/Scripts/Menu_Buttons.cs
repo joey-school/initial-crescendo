0e120c6226750cff276eb53191537b5d28765d24
D. Bakhuis <daryl.bakhuis@gmail.com>
2019-09-10 09:02:18 +0200

    Added tentative Main Menu

---
+++ Assets/Assets (Daryl)/Scripts/Menu_Buttons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Buttons : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("DarylScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
