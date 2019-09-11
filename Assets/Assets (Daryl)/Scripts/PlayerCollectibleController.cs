a7b26a815aaa81181a9b2ef1bc5c20b411e7866f
Unknown <daryl.bakhuis@gmail.com>
2019-09-11 11:45:04 +0200

    Added Simple Collectible System

---
+++ Assets/Assets (Daryl)/Scripts/PlayerCollectibleController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Crescendo.InitialCrescendo
+{
+    public class PlayerCollectibleController : MonoBehaviour
+    {
+
+        public delegate void DieEventHandler();
+        public static event DieEventHandler Died;
+        
+        [SerializeField]
+        private ScoreManager scoreManager;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            Debug.Log(collision.tag, this);
+
+            switch (collision.tag)
+            {
+                case "Collectible":
+                    Destroy(collision.gameObject);
+                    scoreManager.Score++;
+                    break;
+            }
+        }
+    }
+}
