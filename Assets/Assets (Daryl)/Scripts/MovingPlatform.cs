3c745d2517c23b898dfa115fc08a7464cc18b96f
Unknown <daryl.bakhuis@gmail.com>
2019-09-17 13:53:01 +0200

    Added timer to moving platform

--- Assets/Assets (Daryl)/Scripts/MovingPlatform.cs
+++ Assets/Assets (Daryl)/Scripts/MovingPlatform.cs
@@ -7,13 +7,24 @@ public class MovingPlatform : MonoBehaviour
 {
     [SerializeField] private Transform endpoint;
     [SerializeField] private float duration;
+    [SerializeField] private bool platformTimed; //if true this platform will move after 'timer' amount of seconds
+    [SerializeField] private float timer;
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Moving");
         if (collision.tag == "Player")
         {
-            transform.DOMove(endpoint.position, duration);
+            if(droppingPlatform) 
+            {
+                Invoke("MovePlatform", timer);
+            } else {
+                MovePlatform();
+            }
         }
+    }
+
+    private void MovePlatform()
+    {
+        transform.DOMove(endpoint.position, duration);
     }
 }
