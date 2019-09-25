01a3745287c920db65fbb01609309928d3be386c
DOverbeeke <donaldoverbeeke@hotmail.com>
2019-09-25 18:57:52 +0200

    First collectable + platform fixes

---
+++ Assets/Donald/Scripts/RhythmLines/DrawRhythm.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawRhythm : MonoBehaviour
+{
+	[SerializeField] ClickToDrawLine linesBase;
+	//[SerializeField] Vector3 positionFirstLine;
+	[SerializeField] GameObject linePrefab;
+	[SerializeField] Transform parent;
+	[SerializeField] int desiredLineCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		float xDifference = (linesBase.LastPosition.x - linesBase.FirstPosition.x) / linesBase.ClickCount;
+		for(int i = 0; i < desiredLineCount; i++) {
+			Instantiate(linePrefab, Vector3.right * xDifference * i, Quaternion.identity, parent);
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
