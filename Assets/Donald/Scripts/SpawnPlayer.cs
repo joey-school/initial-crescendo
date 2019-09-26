cb5ee32d41115dfb13d508907285d5fa2054bdd7
DOverbeeke <donaldoverbeeke@hotmail.com>
2019-09-26 15:28:49 +0200

    CheckpointMaker for game design

---
+++ Assets/Donald/Scripts/SpawnPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Crescendo.InitialCrescendo
+{
+	public class SpawnPlayer : MonoBehaviour
+	{
+		public Transform SpawnPoint;
+
+		void Start() {
+			transform.position = SpawnPoint.position;
+		}
+	}
+}
