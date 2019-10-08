7850b2dbf67f0aa9ec7f61ef9cc62aa4b93ef0ff
Unknown <daryl.bakhuis@gmail.com>
2019-10-08 16:10:06 +0200

    Temp push of menus

--- Assets/Assets (Daryl)/Scripts/Menu_Buttons.cs
+++ Assets/Assets (Daryl)/Scripts/Menu_Buttons.cs
@@ -8,6 +8,12 @@ namespace Crescendo.InitialCrescendo
     public class Menu_Buttons : MonoBehaviour
     {
         [SerializeField] private string SceneName;
+        [SerializeField]
+        private GameObject
+            LevelSelectPanel,
+            OptionsPanel,
+            MenuPanel,
+            CheatsPanel;
         private bool Pressed;
 
         void Start()
@@ -33,12 +39,38 @@ namespace Crescendo.InitialCrescendo
 
         public void StartGame()
         {
+            SoundManager.Instance.PlaySoundFX(Sounds.StartGame);
             Pressed = true;
         }
 
-        public void QuitGame()
+        public void MainMenuActive()
         {
-            Application.Quit();
+            SoundManager.Instance.PlaySoundFX(Sounds.Pause);
+            MenuPanel.SetActive(true);
+            OptionsPanel.SetActive(false);
+            LevelSelectPanel.SetActive(false);
+            CheatsPanel.SetActive(false);
+        }
+
+        public void OptionsActive()
+        {
+            SoundManager.Instance.PlaySoundFX(Sounds.Pause);
+            MenuPanel.SetActive(false);
+            OptionsPanel.SetActive(true);
+        }
+
+        public void LevelSelectActive()
+        {
+            SoundManager.Instance.PlaySoundFX(Sounds.Pause);
+            MenuPanel.SetActive(false);
+            LevelSelectPanel.SetActive(true);
+        }
+
+        public void CheatsActive()
+        {
+            SoundManager.Instance.PlaySoundFX(Sounds.Pause);
+            OptionsPanel.SetActive(false);
+            CheatsPanel.SetActive(true);
         }
     }
 }
